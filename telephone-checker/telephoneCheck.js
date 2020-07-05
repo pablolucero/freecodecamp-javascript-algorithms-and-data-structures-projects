c31bee0b959d2b44f8171aaa4600dff1604359ea
Pablo Lucero <plucero87@gmail.com>
2020-07-05 15:45:28 -0300

    add telephone checker

--- telephone-checker/telephoneCheck.js
+++ telephone-checker/telephoneCheck.js
@@ -3,25 +3,27 @@ module.exports = function telephoneCheck(aPhoneString) {
   if (typeof aPhoneString !== 'string') {
     throw new Error('Phone number input must be a string');
   }
-
-  const validRegexCollection = [
-    /^1?\d{10}$/,
-    /^1?\d{3}-\d{3}-\d{4}$/,
-    /^1?\(\d{3}\)\d{3}-\d{4}$/,
-  ];
-
-  return checkIfIsValid(validRegexCollection, removeWhiteSpace(aPhoneString));
+  return textMatchWithAny(removeWhiteSpace(aPhoneString), getValidRegexCollection());
 };
+
+function getValidRegexCollection() {
+  // in all cases the number can start (or not) with the country code of 1
+  return [
+    /^1?\d{10}$/,                 // all digits 5555555555
+    /^1?\d{3}-\d{3}-\d{4}$/,      // 555-555-5555
+    /^1?\(\d{3}\)\d{3}-\d{4}$/,   // (555)555-5555
+  ];
+}
 
 function removeWhiteSpace(aPhoneString) {
   return aPhoneString.replace(/ /g, '');
 }
 
-function checkIfIsValid(validRegexCollection, normalizedPhoneString) {
+function textMatchWithAny(aString, aCollectionOfRegex) {
   let isValid = false;
 
-  for (const regExp of validRegexCollection) {
-    if (regExp.test(normalizedPhoneString)) {
+  for (const regExp of aCollectionOfRegex) {
+    if (regExp.test(aString)) {
       isValid = true;
       break;
     }
