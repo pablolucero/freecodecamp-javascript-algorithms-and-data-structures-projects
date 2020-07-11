22224fb4dffc7bf3e5f850d182dcb7f7e5ddf56b
Pablo Lucero <plucero87@gmail.com>
2020-07-10 23:43:27 -0300

    convertToRoman - works up to 3999

--- roman-numeral-converter/convertToRoman.js
+++ roman-numeral-converter/convertToRoman.js
@@ -1,24 +1,3 @@
-function findRomanNumber(result, num, baseNumber) {
-  result += getRomanSymbol(baseNumber);
-  result += concatISymbolNTimes(num - baseNumber);
-  return result;
-}
-
-function getRomanSymbol(number) {
-  const numberToRomanSymbolMap = {
-    1: "I",
-    5: "V",
-    10: "X",
-    50: "L",
-    100: "C",
-    500: "D",
-    1000: "M",
-  };
-
-  return numberToRomanSymbolMap[number];
-}
-
-
 /**
  * 1 ➔ I
  * 2 ➔ II
@@ -40,74 +19,79 @@ module.exports = function convertToRoman(input) {
     throw new Error('Negative numbers not defined');
   }
 
-  if (input % 10 === 4) {
-    if (input === 4) return 'IV';
-    if (input === 14) return 'XIV';
-    if (input === 24) return 'XXIV';
-    if (input === 34) return 'XXXIV';
-  }
-  if (input % 10 === 9) {
-    if (input === 9) return 'IX';
-    if (input === 19) return 'XIX';
-    if (input === 29) return 'XXIX';
-    if (input === 39) return 'XXXIX';
+  let romanResult = '';
+
+  if (input >= 1000) {
+    romanResult += concatSymbolNTimes("M", Math.floor(input / 1000));
+    input = input % 1000;
   }
 
+  if (input >= 900) {
+    romanResult += "CM";
+    input -= 900;
+  }
+
+  if (input >= 500) {
+    romanResult += concatSymbolNTimes("D", Math.floor(input / 500));
+    input = input % 500;
+  }
+
+  if (input >= 400) {
+    romanResult += "CD";
+    input -= 400;
+  }
+
+  if (input >= 100) {
+    romanResult += concatSymbolNTimes("C", Math.floor(input / 100));
+    input = input % 100;
+  }
+
+  if (input >= 90) {
+    romanResult += "XC";
+    input -= 90;
+  }
+
+  if (input >= 50) {
+    romanResult += concatSymbolNTimes("L", Math.floor(input / 50));
+    input = input % 50;
+  }
+
+  if (input >= 40) {
+    romanResult += "XL";
+    input -= 40;
+  }
+
+  if (input >= 10) {
+    romanResult += concatSymbolNTimes("X", Math.floor(input / 10));
+    input = input % 10;
+  }
+
+  if (input === 9) {
+    romanResult += "IX";
+    input -= 9;
+  }
+
+  if (input >= 5) {
+    romanResult += "V";
+    input -= 5;
+  }
+
+  if (input === 4) {
+    romanResult += "IV";
+    input -= 4;
+  }
+
+  if (input >= 1) {
+    romanResult += concatSymbolNTimes('I', input);
+  }
+
+  return romanResult;
+};
+
+function concatSymbolNTimes(symbol, times) {
   let result = '';
-
-  if (input >= 1 && input <= 3) {
-    result = findRomanNumber(result, input, 1);
-  }
-
-  if (input >= 5 && input <= 8) {
-    result = findRomanNumber(result, input, 5);
-  }
-
-  if (input >= 10 && input <= 13) {
-    result = findRomanNumber(result, input, 10);
-  }
-
-  if (input >= 15 && input <= 18) {
-    result += 'XV';
-    result += concatISymbolNTimes(input - 15);
-  }
-
-  if (input >= 20 && input <= 23) {
-    result += 'XX';
-    result += concatISymbolNTimes(input - 20);
-  }
-
-  if (input >= 25 && input <= 28) {
-    result += 'XXV';
-    result += concatISymbolNTimes(input - 25);
-  }
-
-  if (input >= 30 && input <= 33) {
-    result += 'XXX';
-    result += concatISymbolNTimes(input - 30);
-  }
-
-  if (input >= 35 && input <= 38) {
-    result += 'XXXV';
-    result += concatISymbolNTimes(input - 35);
-  }
-
-  if (input === 40) return 'XL';
-
-  if (input === 50) return 'L';
-  if (input === 100) return 'C';
-  if (input === 500) return 'D';
-  if (input === 1000) return 'M';
-
-
-  return result;
-}
-;
-
-function concatISymbolNTimes(num) {
-  let result = '';
-  for (let i = 0; i < num; i++) {
-    result += 'I';
+  for (let i = 0; i < times; i++) {
+    result += symbol;
   }
   return result;
 }
