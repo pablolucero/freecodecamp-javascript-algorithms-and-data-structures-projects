98b312d27bbed0e79aeb4951e0b4cd5c78eccd1e
Pablo Lucero <plucero87@gmail.com>
2020-07-11 00:26:59 -0300

    convertToRoman - refactor to generalize the algorithm into a for loop

--- roman-numeral-converter/convertToRoman.js
+++ roman-numeral-converter/convertToRoman.js
@@ -1,88 +1,21 @@
 /**
- * 1 ➔ I
- * 2 ➔ II
- * 3 ➔ III
- * 4 ➔ IV
- * 5 ➔ V
- * 9 ➔ IX
- * 21 ➔ XXI
- * 50 ➔ L
- * 100 ➔ C
- * 500 ➔ D
- * 1000 ➔ M
+ * Return the given number roman numeral version in upper-case
+ *
+ * @param {number} input: a number from 1 to 3999
  */
 module.exports = function convertToRoman(input) {
-  if (input === 0) {
-    throw new Error('Zero not defined');
-  }
-  if (input < 0) {
-    throw new Error('Negative numbers not defined');
-  }
+  validateInput(input);
+
+  const values = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
+  const symbols = ['M', 'CM', 'D', 'CD', 'C', 'XC', 'L', 'XL', 'X', 'IX', 'V', 'IV', 'I'];
 
   let romanResult = '';
 
-  if (input >= 1000) {
-    romanResult += concatSymbolNTimes('M', Math.floor(input / 1000));
-    input = input % 1000;
-  }
-
-  if (input >= 900) {
-    romanResult += 'CM';
-    input -= 900;
-  }
-
-  if (input >= 500) {
-    romanResult += concatSymbolNTimes('D', Math.floor(input / 500));
-    input = input % 500;
-  }
-
-  if (input >= 400) {
-    romanResult += 'CD';
-    input -= 400;
-  }
-
-  if (input >= 100) {
-    romanResult += concatSymbolNTimes('C', Math.floor(input / 100));
-    input = input % 100;
-  }
-
-  if (input >= 90) {
-    romanResult += 'XC';
-    input -= 90;
-  }
-
-  if (input >= 50) {
-    romanResult += concatSymbolNTimes('L', Math.floor(input / 50));
-    input = input % 50;
-  }
-
-  if (input >= 40) {
-    romanResult += 'XL';
-    input -= 40;
-  }
-
-  if (input >= 10) {
-    romanResult += concatSymbolNTimes('X', Math.floor(input / 10));
-    input = input % 10;
-  }
-
-  if (input === 9) {
-    romanResult += 'IX';
-    input -= 9;
-  }
-
-  if (input >= 5) {
-    romanResult += 'V';
-    input -= 5;
-  }
-
-  if (input === 4) {
-    romanResult += 'IV';
-    input -= 4;
-  }
-
-  if (input >= 1) {
-    romanResult += concatSymbolNTimes('I', input);
+  for (let i = 0; i < values.length; i++) {
+    if (input >= values[i]) {
+      romanResult += concatSymbolNTimes(symbols[i], Math.floor(input / values[i]));
+      input = input % values[i];
+    }
   }
 
   return romanResult;
@@ -90,8 +23,19 @@ module.exports = function convertToRoman(input) {
 
 function concatSymbolNTimes(symbol, times) {
   let result = '';
+
   for (let i = 0; i < times; i++) {
     result += symbol;
   }
+
   return result;
+}
+
+function validateInput(number) {
+  if (number === 0) {
+    throw new Error('Zero not defined');
+  }
+  if (number < 0) {
+    throw new Error('Negative numbers not defined');
+  }
 }
